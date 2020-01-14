08146d4ee7b480fac83543b82c2682a12778d622
Ericgoodboy <776968336@qq.com>
2020-01-14 08:53:45 +0800

    01140853

--- csdn/pipelines.py
+++ csdn/pipelines.py
@@ -5,10 +5,24 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import logging
+from scrapy.exporters import JsonLinesItemExporter
+
 logger = logging.getLogger(__name__)
 
 class CsdnPipeline(object):
+    def __init__(self):
+        self.fp = open("data.json","wb")
+        self.expoter = JsonLinesItemExporter(self.fp,ensure_ascii=False)
+        self.count = 1
+        pass
+
     def process_item(self, item, spider):
         logging.debug(">"*30)
-        logging.debug(item)
-        pass
+        self.count+=1
+        self.expoter.export_item(item)
+        # logging.debug(item)
+        if self.count>100:
+            exit(0)
+
+    def __del__(self):
+        self.fp.close()
--- csdn/spiders/csdnSpyder.py
+++ csdn/spiders/csdnSpyder.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+
 import logging
 from csdn.items import CsdnItem
 
@@ -9,15 +10,14 @@ class CsdnspyderSpider(scrapy.Spider):
     name = 'csdnSpyder'
     allowed_domains = ['blog.csdn.net']
     start_urls = ['https://blog.csdn.net/u014427391/article/details/102785219']
-
     def parse(self, response):
         for i in response.xpath("//div[@class='content']/a/@href"):
-            logger.debug(i.get())
-            pass
+            next_url=i.get()
+            if next_url:
+                yield scrapy.Request(next_url,self.parse)
         tags = []
         for i in response.xpath("//a[@class='tag-link']/text()"):
             tag = i.get().strip().lstrip()
             tags.append(tag)
         item = CsdnItem(tags=tags, url=response.url, content=response.xpath("//div[@id='content_views']").get())
         yield item
-        
